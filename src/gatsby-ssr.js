fcda7e38efc958f8b9fce8179c82662cf7763bf2
Kevin Cullen <kevincullen@Kevin-Cullens-MacBook-Pro.local>
2019-08-29 16:00:30 -0500

    Add multiple pixel Ids

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -12,7 +12,8 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
   t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
   document,'script','https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '${pluginOptions.pixelId}'); // Insert your pixel ID here.
+  fbq('init', '${pluginOptions.pixelIdOne}'); // Insert your pixel ID here.
+  fbq('init', '${pluginOptions.pixelIdTwo}'); // Insert your pixel ID here.
   fbq('track', 'PageView');
       `,
         }}
