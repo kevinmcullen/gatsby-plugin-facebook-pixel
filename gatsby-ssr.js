39395b96fb8c41000dd4dfd7f49835e32e851a0e
Kevin Cullen <kevincullen@Kevin-Cullens-MacBook-Pro.local>
2019-08-30 13:04:27 -0500

    fixed build

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -13,7 +13,7 @@ exports.onRenderBody = function (_ref, pluginOptions) {
     return setHeadComponents([_react2.default.createElement('script', {
       key: 'gatsby-plugin-facebook-pixel',
       dangerouslySetInnerHTML: {
-        __html: '\n  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?\n  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;\n  n.push=n;n.loaded=!0;n.version=\'2.0\';n.queue=[];t=b.createElement(e);t.async=!0;\n  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,\n  document,\'script\',\'https://connect.facebook.net/en_US/fbevents.js\');\n  fbq(\'init\', \'' + pluginOptions.pixelId + '\'); // Insert your pixel ID here.\n  fbq(\'track\', \'PageView\');\n      '
+        __html: '\n  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?\n  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;\n  n.push=n;n.loaded=!0;n.version=\'2.0\';n.queue=[];t=b.createElement(e);t.async=!0;\n  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,\n  document,\'script\',\'https://connect.facebook.net/en_US/fbevents.js\');\n  fbq(\'init\', \'' + pluginOptions.pixelIdOne + '\'); // Insert your pixel ID here.\n  fbq(\'init\', \'' + pluginOptions.pixelIdTwo + '\'); // Insert your pixel ID here.\n  fbq(\'track\', \'PageView\');\n      '
       }
     })]);
   }
